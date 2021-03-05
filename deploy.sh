8f0dba1137af249d9119c89df23802f82ef10a0b
SamedhG <gupta.sam@husky.neu.edu>
2021-03-05 14:14:14 -0500

    fix script 2

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,14 @@
 export MIX_ENV=prod
 export SECRET_KEY_BASE=insecure
 export PORT=4820
-export DATABASE_URL=ecto://events_db:no_pass/events
+DB_PASS=`pwd/cfg/.db_pass`
+
+if [ ! -e "$DB_PASS" ]; then
+    echo "Setup the database first"
+    exit 1
+fi
+
+export DATABASE_URL=ecto://events_db:`cat $DB_PASS`/events
 
 mix deps.get --only prod
 mix compile
@@ -15,16 +22,10 @@ if [ ! -e "$KEY" ]; then
     mix phx.gen.secret > "$KEY"
 fi
 
-DB_PASS=`pwd/cfg/.db_pass`
 
-if [ ! -e "$DB_PASS" ]; then
-    echo "Setup the database first"
-    exit 1
-fi
 
 SECRET_KEY_BASE=$(cat "$KEY")
 export SECRET_KEY_BASE
-export DATABASE_URL=ecto://events_db:`cat $DB_PASS`/events
 
 
 
